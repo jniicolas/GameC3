c04c1e707e375ce48227f39f0d59ba6b0afee77b
jniicolas <90976591+jniicolas@users.noreply.github.com>
2021-12-09 22:08:31 -0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,48 @@
+function jogar () {
+    let cores = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"]
+    let dezCores: Array<string> = [];
+    let corSorteada = null
+    let corUsuario = null
+    let vidas = 5
+    //var res = document.querySelector('p#res')
+
+    for (let i = 0; i < 10 ; i++) {
+        dezCores.push(cores[Math.floor(Math.random() * cores.length) +1].toLowerCase())
+        dezCores.sort()
+        corSorteada = dezCores[Math.floor((Math.random() * dezCores.length) + 1)]
+    }
+   
+    do{
+        corUsuario = prompt(`Eu estou pensando em uma dessas cores:\n\n${dezCores.join(' | ')}\n\nQual cor eu estou pensando ?\n\nVidas: ${vidas}`).toLowerCase()
+
+        if (corUsuario.length === 0 || !corUsuario.trim()) { //verifica se está vazio
+            alert('Por favor, digite uma cor!!')
+        } 
+        else if (!dezCores.includes(corUsuario)) { //verificar se a cor existe
+            alert('Essa cor não está entre as 10 possiveis ou não existe!\n\nTente novamente')
+        } 
+        else if (corUsuario != corSorteada) { //verifica se o usuário acertou o valor ou não
+            verificar(corSorteada, corUsuario, vidas)
+            vidas--
+        } 
+        else if (corUsuario == corSorteada){
+            document.body.style.backgroundColor = corUsuario
+            alert('Parabéns, você acertou!!')
+            //res.innerHTML = 'Obrigado por jogar!!!'
+            vidas = 0
+        }
+    }while (vidas != 0);
+
+    function verificar(a: string,b: string,v: number) { //verificar a possição da cor esclhida para usuário para dar a dica
+        if (a.charCodeAt(0) > b.charCodeAt(0)) {
+            if (v == 1) {
+                alert(`Desculpe, mas suas vidas acabaram!\n\nA correta era: "${a}"\n\nObrigado por jogar!!!`)
+                //res.innerHTML = 'Atualize a página ou dê um F5 para jogar novamente.'
+            } else {
+                alert('Você errou!!\n\nDica: Sua cor é alfabéticamente menor que a minha\n\nPor favor, tente novamente!')
+            }
+        } else {
+            alert('Você errou!!\n\nDica: Sua cor é alfabéticamente maior que a minha\n\nPor favor, tente novamente!')
+        }
+    }
+}
